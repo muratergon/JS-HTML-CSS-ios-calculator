dd168c7a904edd9e29ab4587aebde963d0083988
murat <ergon.muratt@gmail.com>
2022-10-27 20:38:18 +0300

    css-js

--- script.js
+++ script.js
@@ -1,27 +1,170 @@
-const screen2 = document.querySelector(".screen2");
-const screen1 = document.querySelector(".screen1");
-const keys = document.querySelector(".calculator-keys");
+let firstScreen = document.querySelector(".firstScreen");
+let secondScreen = document.querySelector(".secondScreen");
+let buttons = document.querySelector(".buttons");
 
-keys.addEventListener("click", (e) => {
-  if (e.target.classList.contains("number")) {
-    screen2.innerText += e.target.value;
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("buttons")) {
+    null;
+  } else {
+    if (e.target.classList.contains("ac")) {
+      e.target.closest(".container").querySelector(".firstScreen").innerText =
+        "";
+      e.target.closest(".container").querySelector(".secondScreen").innerText =
+        "";
+    } else if (e.target.classList.contains("num")) {
+      if (
+        secondScreen.innerText == 0 &&
+        secondScreen.innerText.match(/\./) == null
+      ) {
+        secondScreen.innerHTML = e.target.innerText;
+      } else {
+        if (secondScreen.innerText.length <= 6) {
+          secondScreen.innerText += e.target.innerText;
+        }
+      }
+    } else if (e.target.classList.contains("dot")) {
+      if (
+        secondScreen.innerText &&
+        secondScreen.innerText.match(/\./) == null &&
+        secondScreen.innerText.length <= 5
+      ) {
+        secondScreen.innerText += e.target.innerText;
+      }
+    } else if (e.target.classList.contains("operator")) {
+      if (
+        firstScreen.innerText.length == 0 &&
+        secondScreen.innerText &&
+        secondScreen.innerText.match(/[\.]$/) == null
+      ) {
+        secondScreen.innerText += e.target.innerText;
+        firstScreen.innerText = secondScreen.innerText;
+        secondScreen.innerText = "";
+      } else if (firstScreen.innerText && secondScreen.innerText.length == 0) {
+        firstScreen.innerText = firstScreen.innerText.replace(
+          firstScreen.innerText[firstScreen.innerText.length - 1],
+          e.target.innerText
+        );
+      } else if (
+        firstScreen.innerText &&
+        secondScreen.innerText &&
+        secondScreen.innerText.endsWith(".") == false
+      ) {
+        operationSecond();
+        firstScreen.innerText += e.target.innerText;
+      }
+    } else if (e.target.classList.contains("equal")) {
+      if (
+        secondScreen.innerText &&
+        firstScreen.innerText &&
+        secondScreen.innerText.match(/[\.]$/) == null
+      ) {
+        operation();
+      }
+    } else if (e.target.classList.contains("plusminus")) {
+      if (secondScreen.innerText != 0) {
+        secondScreen.innerText = "-1" * secondScreen.innerText;
+      }
+    } else if (e.target.classList.contains("percent")) {
+      if (
+        firstScreen.innerText == "" &&
+        secondScreen.innerText.match(/[.]$/) == null
+      ) {
+        secondScreen.innerText = Number(secondScreen.innerText) / 100;
+      }
+    }
   }
-  if (e.target.classList.contains("operator")) {
-    screen2.innerText += e.target.value;
-    screen1.innterText = screen2.innterText;
-    screen2.innterText = " ";
-  }
-
-  //   if (e.target.classList.contains("plus")) {
-  //     screen.innerText += e.target.value;
-  //   }
-  //   if (e.target.classList.contains("minus")) {
-  //     screen.innerText += e.target.value;
-  //   }
-  //   if (e.target.classList.contains("divide")) {
-  //     screen.innerText += e.target.value;
-  //   }
-  //   if (e.target.classList.contains("multi")) {
-  //     screen.innerText += e.target.value;
-  //   }
 });
+
+function operation() {
+  switch (firstScreen.innerText[firstScreen.innerText.length - 1]) {
+    case "+":
+      secondScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) + Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      firstScreen.innerText = "";
+      break;
+    case "-":
+      secondScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) - Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      firstScreen.innerText = "";
+      break;
+    case "x":
+      secondScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) * Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      firstScreen.innerText = "";
+      break;
+    case "/":
+      secondScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) / Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      firstScreen.innerText = "";
+      break;
+    default:
+      break;
+  }
+}
+
+function operationSecond() {
+  switch (firstScreen.innerText[firstScreen.innerText.length - 1]) {
+    case "+":
+      firstScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) + Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      secondScreen.innerText = "";
+      break;
+    case "-":
+      firstScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) - Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      secondScreen.innerText = "";
+      break;
+    case "x":
+      firstScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) * Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      secondScreen.innerText = "";
+      break;
+    case "/":
+      firstScreen.innerText = Number(
+        (
+          Number(
+            firstScreen.innerText.slice(0, firstScreen.innerText.length - 1)
+          ) / Number(secondScreen.innerText)
+        ).toFixed(4)
+      );
+      secondScreen.innerText = "";
+      break;
+    default:
+      break;
+  }
+}
